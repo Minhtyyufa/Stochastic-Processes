a16826aceca78cac3c06f74f5f8f23d6918edbe7
Do Hyung Kwon <thedavekwon@gmail.com>
2020-04-17 17:12:45 -0400

    Detection

--- Detection proj/iris.py
+++ Detection proj/iris.py
@@ -2,28 +2,77 @@ import scipy.io as sio
 import numpy as np
 from scipy.stats import multivariate_normal
 from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+# load the data from matlab mat file
 iris = sio.loadmat("Iris.mat")
 y = iris['labels']
 X = iris['features']
 
+def L(pdf, j, y):
+    return pdf[j].pdf(y)/pdf[1].pdf(y)
+
+
+# Defines the inequalities that classify which group the observation is in, then returns that group. 
+# 3-ary MPE Classifier with MAP Rule
+def classify(pdf, P1, P2, P3, y):
+    L2 = L(pdf, 2, y)
+    L3 = L(pdf, 3, y)
+    
+    # Equations 8.96-8.98
+    comp12 = P2*L2 > P1
+    comp23 = -P2*L2 > -P3*L3
+    comp31 = 0 > -P1 + P3*L3
+    
+    if not comp31 and comp23:
+        return 3
+    if comp31 and not comp12:
+        return 1
+    if comp12 and not comp23:
+        return 2
+    
+# split into test and train 50 to 50
 skf = StratifiedKFold(n_splits=2, shuffle=True)
 for train_index, test_index in skf.split(X, y):
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
     
+    # find the prior probability
+    P1 = np.count_nonzero(y_train == 1)/len(y_train)
+    P2 = np.count_nonzero(y_train == 2)/len(y_train)
+    P3 = np.count_nonzero(y_train == 3)/len(y_train)
+    
     data = {}
     pdf = {}
-    for label in np.unique(y_train):
+    
+    labels = np.unique(y_train)
+    
+    for label in labels:
         data[label] = []
     
     for i in range(len(X_train)):
         data[y_train[i][0]].append(X_train[i])
     
-    for label in np.unique(y_train):
+    # find the conditional pdf for likelihood
+    for label in labels:
         data[label] = np.array(data[label])
         size = len(data[label][0])
         label_pdf = multivariate_normal(mean=np.mean(data[label], axis=0), cov=np.cov(np.transpose(data[label])))
         pdf[label] = label_pdf
+    
+    # use m-ary classifier with MAP rule
+    y_hat = [[classify(pdf, P1, P2, P3, X_test[i])] for i in range(len(X_test))]
+    print((len(y_hat)-np.sum(y_hat == y_test))/len(y_hat))
+    print(confusion_matrix(y_hat, y_test))
+    
+    # plot the confusion matrix
+    labels = range(1,4)
+    sns.heatmap(confusion_matrix(y_hat,y_test), cmap="YlGnBu", annot=True, fmt="d", yticklabels=labels, xticklabels=labels)
+    plt.title('Confusion Matrix for Iris Classifier')
+    plt.show()
+    
+
     
     
--- Detection proj/radar.py
+++ Detection proj/radar.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 
+# Defining Constants
 STDs = range(1, 3) # W
 As = range(1, 3) # E
 zs = [2, 10]
@@ -9,21 +10,24 @@ N = 1000
 P0 = 0.8
 P1 = 1 - P0
 
-
+# Plotting the ROC given a standard deviation, threshold, and distributions
 def findROC(beta, A, STD, Y, A_vec):
-    Y_dec = beta
+    Y_dec = beta # Decision Threshold
     Y1 = (Y > Y_dec) 
     Y0 = (Y < Y_dec)
     # part 1 0.8 * 0.0296 (X > Y_dec) P(X > eta)
     # part 2 0.2 * 0.8123 (X < Y_dec) P(X+A < eta)
     
     # Two parts to the error: P(Deciding detected when not present) + P(Deciding not detected when present)
-    theo_prob = (1-norm.cdf(Y_dec/STD))*P0 + (norm.cdf((Y_dec-A)/STD))*P1
+    theo_prob = (1-norm.cdf(Y_dec/STD))*P0 + (norm.cdf((Y_dec-A)/STD))*P1  # Equation (8.36)
     print("theoretical probability error: {}".format(theo_prob))
     print("measured probability error: {}".format(np.sum(np.logical_xor(A_vec, Y1))/N))
     
-    # part b
-    # TP FP
+    # part b for ROC curve
+    # True Positive
+    # False Positive
+    # True Negative
+    # False Negative
     TP = np.sum(A_vec*Y1)/N
     FP = np.sum(np.logical_not(A_vec)*Y1)/N
     TN = np.sum(np.logical_not(A_vec)*Y0)/N
@@ -31,21 +35,29 @@ def findROC(beta, A, STD, Y, A_vec):
     
     return TP, FP
     
+legends = []
 #for STD, A in zip(STDs, As):
 for STD in STDs:
     for A in As:
-        A_vec = np.random.uniform(size=1000) < P1
-        X = np.random.normal(0, STD, N)
+        legends.append("STD: {}, A: {}".format(STD, A))
+        legends.append("MPE STD: {}, A: {}".format(STD, A))
+        legends.append("Adjusted Cost STD: {}, A: {}".format(STD, A))
+        A_vec = np.random.uniform(size=1000) < P1  
+        X = np.random.normal(0, STD, N)  # Gaussian Noise
         Y = A*A_vec + X
         
         # MPE part a
+        # Defining costs for MAP
         C00 = 0 
         C01 = 1
         C10 = 1
         C11 = 0
         
+
         TPs = []
         FPs = []
+
+        # looping over the decision values and finding the ROC
         for beta in np.linspace(-8, 8):
             TP, FP = findROC(beta=beta,
                              A=A,
@@ -64,11 +76,15 @@ for STD in STDs:
                          STD=STD,
                          Y=Y,
                          A_vec=A_vec)
-
+        
 
         # part c
+        
+        # New Cost Values
         C10 = 1
         C01 = 10
+        
+        # Updated eta for the new costs and finding the new ROC
         eta = ((C10-C00)*P0)/((C01-C11)*P1)
         Y_dec= A/2 + STD**2*np.log(eta)/A
         TP_changed_cost, FP_changed_cost = findROC(beta=Y_dec,
@@ -80,12 +96,20 @@ for STD in STDs:
         plt.plot(FPs, TPs, 'o-')
         # MLE
         plt.plot(FP, TP, '*', markersize=16)
+        
         # Part C adjusted cost MLE
         plt.plot(FP_changed_cost, TP_changed_cost, '^', markersize=16)
+
+# Plot Format
+plt.legend(legends)
+plt.xlabel('PF')
+plt.ylabel('PD')
+plt.title('ROC of LRT')        
 plt.show()
 
 
 # Part D
+# P1s changes the prior probability from 0.01 to 0.99
 P1s = np.linspace(0.01, 0.99, 100)
 expecteds = []
 A = 1
@@ -94,16 +118,27 @@ C10 = 1
 C01 = 10
 C00 = 0
 C11 = 0
+legends =[]
+
 for P1 in P1s:
+    # Adding Legend to the ROC Plot
+    legends.append("P1: {}".format(P1))
+
+    # Generate Distributions
     A_vec = np.random.uniform(size=1000) < P1
     X = np.random.normal(0, STD, N)
     Y = A*A_vec + X
     
+    # Eta Calcuation
     eta = ((C10-C00)*(1-P1))/((C01-C11)*P1)
-    
+    # Y_dec calculation
     Y_dec= A/2 + STD**2*np.log(eta)/A
+
+    # Apply Decision Rules
     Y1 = (Y > Y_dec) 
     Y0 = (Y < Y_dec)
+
+    # Calculating Statistics
     TP = np.sum(A_vec*Y1)/N
     FP = np.sum(np.logical_not(A_vec)*Y1)/N
     TN = np.sum(np.logical_not(A_vec)*Y0)/N
@@ -111,11 +146,15 @@ for P1 in P1s:
 
     # C00 = 0, C11 = 0
     expecteds.append(FP*C01 + FN*C10)
+# Plot Format
 plt.plot(P1s, expecteds, '-o')
+plt.xlabel('A Priori Probability')
+plt.ylabel('Expected Cost')
+plt.title('Expected Cost vs a Priori Possibility')
+plt.legend(legends)
 plt.show()
 
-# E
-
+# Part E
 # beta = +/- sqrt(gamma)
 def findROC2(beta, A, STD, STDz, Y, A_vec):
     Y_dec = beta+A
@@ -123,16 +162,19 @@ def findROC2(beta, A, STD, STDz, Y, A_vec):
     Y0 = (np.abs(Y-A) > Y_dec-A)
     # part 1 0.8 * 0.0296 (X > Y_dec) P(X > eta)
     # part 2 0.2 * 0.8123 (X < Y_dec) P(X+A < eta) 
-    #Gamma_prime=(2*(STD**2*STDz**2)/(STD**2-STDz**2))*np.log(STD*eta/STDz)
-    # Two parts to the error: P(Deciding detected when not present) + P(Deciding not detected when present)
+    # Gamma_prime=(2*(STD**2*STDz**2)/(STD**2-STDz**2))*np.log(STD*eta/STDz)
 
-    theo_prob = 2*(1-norm.cdf(Y_dec, scale=STDz, loc=A))*P0 + (norm.cdf(Y_dec, scale=STD, loc=A)-(1-norm.cdf(Y_dec, loc=A, scale=STD)))*P1
+    # Two parts to the error: P(Deciding detected when not present) + P(Deciding not detected when present)
+    theo_prob = (norm.cdf(Y_dec, scale=STDz, loc=A)-(1-norm.cdf(Y_dec, loc=A, scale =STDz)))*P0 + 2*(norm.cdf(A-beta, scale=STD, loc=A))*P1
 
     print("theoretical probability error: {}".format(theo_prob))
     print("measured probability error: {}".format(np.sum(np.logical_xor(A_vec, Y1))/N))
     
-    # part b
-    # TP FP
+    # part b for ROC curve
+    # True Positive
+    # False Positive
+    # True Negative
+    # False Negative
     TP = np.sum(A_vec*Y1)/N
     FP = np.sum(np.logical_not(A_vec)*Y1)/N
     TN = np.sum(np.logical_not(A_vec)*Y0)/N
@@ -140,12 +182,20 @@ def findROC2(beta, A, STD, STDz, Y, A_vec):
     
     return TP, FP
     
-P1 = .8
-P0 = .2
-    
+#Instantiate Parameters
+STDs = range(1, 3) # W
+As = range(1, 3) # E
+zs = [2, 10]
+N = 1000
+P0 = 0.8
+P1 = 1 - P0
+legends = []    
+
+# repeats the same process as above, but with the new Z noise
 for z in zs:
     for STD in STDs:
         for A in As:
+            legends.append("STD: {}, A: {}, sig_z/sig_x: {}".format(STD, A, z))
             present = np.random.uniform(size=1000) < P1
             X = np.random.normal(0, STD, N)
             Z = np.random.normal(0, STD*z, N)
@@ -157,10 +207,11 @@ for z in zs:
             C10 = 1
             C11 = 0
             
+            # Makes new arrays for the new observations with Z
             TPs = []
             FPs = []
-            TPzs = []
-            FPzs = []
+        
+            # Loops over the new decision thresholds
             for beta in np.linspace(0, 8):
                 TP, FP = findROC2(beta=beta,
                                  A=A,
@@ -171,7 +222,12 @@ for z in zs:
     
                 TPs.append(TP)
                 FPs.append(FP)
-
+            #Plot ROC
             plt.plot(FPs, TPs, 'o-')
-            plt.plot(FPzs, TPzs, '--')
+
+# Plot Format      
+plt.legend(legends)  
+plt.title('ROC')
+plt.xlabel('PF')
+plt.ylabel('PD')
 plt.show()
